6bb1a8695843e9cd48fc1c8651810ce80a7bd6f2
arobthechief <roberts281@live.marshall.edu>
2016-10-11 20:35:46 -0400

    Add project files.

---
+++ SWEET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SWEET.MvcApplication" Language="C#" %>
---
+++ SWEET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SWEET.Startup))]
+namespace SWEET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
